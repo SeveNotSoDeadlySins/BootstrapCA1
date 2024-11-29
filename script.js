c5831338e252569b9f580a548fafadf9cd00bb26
Tom Mc Carthy (Student) <N00230551@iadt.ie>
2024-11-29 11:37:04 +0000

    changing everything to sass and changing the javascript to add a limit of scrolling

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
 
+const containerHeight = document.querySelector('.custom-container').offsetHeight;
+
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-    text.style.marginTop = value * 2.0 + 'px';
+    let maxScroll = 1200; //Scroll limit
+
+    text.style.marginTop = Math.min(value * 2.0, maxScroll) + 'px';
     leaf.style.top = value * -1.0 + 'px';
     leaf.style.left = value * 1.5 + 'px';
     hill5.style.left = value * 1.5 + 'px';
     hill4.style.left = value * -1.5 + 'px';
-
 });
