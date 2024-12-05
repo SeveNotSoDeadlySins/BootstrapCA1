f92be7d267582e31b32e5b91a7bd54e6081989df
Tom McCarthy <n00230551@iadt.ie>
2024-12-05 20:40:40 +0000

    a mistake on git so forgot one file

--- script.js
+++ script.js
@@ -8,13 +8,7 @@ const containerHeight = document.querySelector('.custom-container').offsetHeight
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-<<<<<<< HEAD
     text.style.marginTop = value * 1.0 + 'px';
-=======
-    let maxScroll = 1200; //Scroll limit
-
-    text.style.marginTop = Math.min(value * 2.0, maxScroll) + 'px';
->>>>>>> c5831338e252569b9f580a548fafadf9cd00bb26
     leaf.style.top = value * -1.0 + 'px';
     leaf.style.left = value * 1.5 + 'px';
     hill5.style.left = value * 1.5 + 'px';
